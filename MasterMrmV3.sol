6b3ade3828abac8bc5fc402e4882ebc58ff77d37
MarmaladeSwap <84860203+MarmaladeSwap@users.noreply.github.com>
2021-09-27 21:05:29 +0300

    Update MasterMrmV3.sol

--- MasterMrmV3.sol
+++ MasterMrmV3.sol
@@ -16,19 +16,19 @@ import './pancake-swap-lib/contracts/token/BEP20/IBEP20.sol';
 import './pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol';
 import './pancake-swap-lib/contracts/access/Ownable.sol';
 
-import "./MrmToken.sol";
-import "./MrmSplitBar.sol";
+import "./MarmToken.sol";
+import "./MarmSplitBar.sol";
 import "./libs/IReferral.sol";
 
-// MasterMrm is the master of MRM AND MRMSPLIT. 
-// He can make Mrm and he is a fair guy.
+// MasterMarm is the master of MARM AND MARMSPLIT. 
+// He can make Marm and he is a fair guy.
 //
 // Note that it's ownable and the owner wields tremendous power. The ownership
-// will be transferred to a governance smart contract once MRM is sufficiently
+// will be transferred to a governance smart contract once MARM is sufficiently
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
-contract MasterMrm is Ownable {
+contract MasterMarm is Ownable {
     using SafeMath for uint256;
     using SafeBEP20 for IBEP20;
 
@@ -37,13 +37,13 @@ contract MasterMrm is Ownable {
         uint256 amount;     // How many LP tokens the user has provided.
         uint256 rewardDebt; // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of MRMs
+        // We do some fancy math here. Basically, any point in time, the amount of MARMs
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accMrmPerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accMarmPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accMrmPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accMarmPerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -52,20 +52,20 @@ contract MasterMrm is Ownable {
     // Info of each pool.
     struct PoolInfo {
         IBEP20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. MRMs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that MRMs distribution occurs.
-        uint256 accMrmPerShare; // Accumulated MRMs per share, times 1e12. See below.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. MARMs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that MARMs distribution occurs.
+        uint256 accMarmPerShare; // Accumulated MARMs per share, times 1e12. See below.
     }
 
-    // The MRM TOKEN!
-    MrmToken public mrm;
-    // The MRMSPLIT TOKEN!
-    MrmSplitBar public mrmsplit;
+    // The MARM TOKEN!
+    MarmToken public marm;
+    // The MARMSPLIT TOKEN!
+    MarmSplitBar public marmsplit;
     // Dev address.
     address public devaddr;
-    // MRM tokens created per block.
-    uint256 public mrmPerBlock;
-    // Bonus muliplier for early mrm makers.
+    // MARM tokens created per block.
+    uint256 public marmPerBlock;
+    // Bonus muliplier for early marm makers.
     uint256 public BONUS_MULTIPLIER;
 
 
@@ -75,44 +75,46 @@ contract MasterMrm is Ownable {
     mapping (uint256 => mapping (address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when MRM mining starts.
+    // The block number when MARM mining starts.
     uint256 public startBlock;
     
     
     // MarmaladeSwap referral contract address.		
     IReferral public referral;		
     // Referral commission rate in basis points.		
-    uint16 public referralCommissionRate = 200;		
+    uint16 public referralCommissionRate = 500;		
     // Max referral commission rate: 5%.		
-    uint16 public constant MAXIMUM_REFERRAL_COMMISSION_RATE = 500;
+    uint16 public constant MAXIMUM_REFERRAL_COMMISSION_RATE = 1000;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event SetReferralAddress(address indexed user, IReferral indexed newAddress);		
     event ReferralCommissionPaid(address indexed user, address indexed referrer, uint256 commissionAmount);
+    event UpdateEmissionRate(address indexed user, uint256 marmPerBlock);
+    event SetStartBlock(address indexed user, uint256 newStartBlock);
 
     constructor(
-        MrmToken _mrm,
-        MrmSplitBar _mrmSplit,
+        MarmToken _marm,
+        MarmSplitBar _marmSplit,
         address _devaddr,
-        uint256 _mrmPerBlock,
+        uint256 _marmPerBlock,
         uint256 _startBlock,
         uint256 _multiplier
     ) public {
-        mrm = _mrm;
-        mrmsplit = _mrmSplit;
+        marm = _marm;
+        marmsplit = _marmSplit;
         devaddr = _devaddr;
-        mrmPerBlock = _mrmPerBlock;
+        marmPerBlock = _marmPerBlock;
         startBlock = _startBlock;
         BONUS_MULTIPLIER = _multiplier;
 
         // staking pool
         poolInfo.push(PoolInfo({
-            lpToken: _mrm,
+            lpToken: _marm,
             allocPoint: 1000,
             lastRewardBlock: startBlock,
-            accMrmPerShare: 0
+            accMarmPerShare: 0
         }));
 
         totalAllocPoint = 1000;
@@ -153,12 +155,12 @@ contract MasterMrm is Ownable {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accMrmPerShare: 0
+            accMarmPerShare: 0
         }));
         updateStakingPool();
     }
 
-    // Update the given pool's MRM allocation point. Can only be called by the owner.
+    // Update the given pool's MARM allocation point. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
         if (_withUpdate) {
             massUpdatePools();
@@ -189,18 +191,18 @@ contract MasterMrm is Ownable {
         return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
-    // View function to see pending MRMs on frontend.
-    function pendingMrm(uint256 _pid, address _user) external view returns (uint256) {
+    // View function to see pending MARMs on frontend.
+    function pendingMarm(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accMrmPerShare = pool.accMrmPerShare;
+        uint256 accMarmPerShare = pool.accMarmPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 mrmReward = multiplier.mul(mrmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accMrmPerShare = accMrmPerShare.add(mrmReward.mul(1e12).div(lpSupply));
+            uint256 marmReward = multiplier.mul(marmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accMarmPerShare = accMarmPerShare.add(marmReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accMrmPerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accMarmPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -224,16 +226,16 @@ contract MasterMrm is Ownable {
             return;
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 mrmReward = multiplier.mul(mrmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        mrm.mint(devaddr, mrmReward.div(10));
-        mrm.mint(address(mrmsplit), mrmReward);
-        pool.accMrmPerShare = pool.accMrmPerShare.add(mrmReward.mul(1e12).div(lpSupply));
+        uint256 marmReward = multiplier.mul(marmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        marm.mint(devaddr, marmReward.div(10));
+        marm.mint(address(marmsplit), marmReward);
+        pool.accMarmPerShare = pool.accMarmPerShare.add(marmReward.mul(1e12).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to MasterMrm for MRM allocation.
+    // Deposit LP tokens to MasterMARM for MARM allocation.
     function deposit(uint256 _pid, uint256 _amount, address _referrer) public validatePool(_pid) {
-        require (_pid != 0, 'deposit MRM by staking');
+        require (_pid != 0, 'deposit MARM by staking');
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
@@ -241,9 +243,9 @@ contract MasterMrm is Ownable {
             referral.recordReferral(msg.sender, _referrer);		
         }
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accMrmPerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accMarmPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
-                safeMrmTransfer(msg.sender, pending);
+                safeMarmTransfer(msg.sender, pending);
                 payReferralCommission(msg.sender, pending);
             }
         }
@@ -251,32 +253,32 @@ contract MasterMrm is Ownable {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accMrmPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accMarmPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
-    // Withdraw LP tokens from MasterMrm.
+    // Withdraw LP tokens from MasterMarm.
     function withdraw(uint256 _pid, uint256 _amount) public validatePool(_pid) {
-        require (_pid != 0, 'withdraw MRM by unstaking');
+        require (_pid != 0, 'withdraw MARM by unstaking');
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
 
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accMrmPerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accMarmPerShare).div(1e12).sub(user.rewardDebt);
         if(pending > 0) {
-            safeMrmTransfer(msg.sender, pending);
+            safeMarmTransfer(msg.sender, pending);
             payReferralCommission(msg.sender, pending);
         }
         if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accMrmPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accMarmPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
-    // Stake MRM tokens to MasterMrm
+    // Stake MARM tokens to MasterMarm
     function enterStaking(uint256 _amount, address _referrer) public {
         PoolInfo storage pool = poolInfo[0];
         UserInfo storage user = userInfo[0][msg.sender];
@@ -285,9 +287,9 @@ contract MasterMrm is Ownable {
             referral.recordReferral(msg.sender, _referrer);
         }
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accMrmPerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accMarmPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
-                safeMrmTransfer(msg.sender, pending);
+                safeMarmTransfer(msg.sender, pending);
                 payReferralCommission(msg.sender, pending);
             }
         }
@@ -295,30 +297,30 @@ contract MasterMrm is Ownable {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accMrmPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accMarmPerShare).div(1e12);
 
-        mrmsplit.mint(msg.sender, _amount);
+        marmsplit.mint(msg.sender, _amount);
         emit Deposit(msg.sender, 0, _amount);
     }
 
-    // Withdraw MRM tokens from STAKING.
+    // Withdraw MARM tokens from STAKING.
     function leaveStaking(uint256 _amount) public {
         PoolInfo storage pool = poolInfo[0];
         UserInfo storage user = userInfo[0][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(0);
-        uint256 pending = user.amount.mul(pool.accMrmPerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accMarmPerShare).div(1e12).sub(user.rewardDebt);
         if(pending > 0) {
-            safeMrmTransfer(msg.sender, pending);
+            safeMarmTransfer(msg.sender, pending);
             payReferralCommission(msg.sender, pending);
         }
         if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accMrmPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accMarmPerShare).div(1e12);
 
-        mrmsplit.burn(msg.sender, _amount);
+        marmsplit.burn(msg.sender, _amount);
         emit Withdraw(msg.sender, 0, _amount);
     }
 
@@ -329,23 +331,23 @@ contract MasterMrm is Ownable {
         pool.lpToken.safeTransfer(address(msg.sender), user.amount);
         emit EmergencyWithdraw(msg.sender, _pid, user.amount);
         if(_pid == 0) {
-                mrmsplit.burn(address(msg.sender), user.amount);
+                marmsplit.burn(address(msg.sender), user.amount);
         }
         user.amount = 0;
         user.rewardDebt = 0;
     }
 
     function getPoolInfo(uint256 _pid) public view
-    returns(address lpToken, uint256 allocPoint, uint256 lastRewardBlock, uint256 accMrmPerShare) {
+    returns(address lpToken, uint256 allocPoint, uint256 lastRewardBlock, uint256 accMarmPerShare) {
         return (address(poolInfo[_pid].lpToken),
             poolInfo[_pid].allocPoint,
             poolInfo[_pid].lastRewardBlock,
-            poolInfo[_pid].accMrmPerShare);
+            poolInfo[_pid].accMarmPerShare);
     }
 
-    // Safe mrm transfer function, just in case if rounding error causes pool to not have enough MRMs.
-    function safeMrmTransfer(address _to, uint256 _amount) internal {
-        mrmsplit.safeMrmTransfer(_to, _amount);
+    // Safe marm transfer function, just in case if rounding error causes pool to not have enough MARMs.
+    function safeMarmTransfer(address _to, uint256 _amount) internal {
+        marmsplit.safeMarmTransfer(_to, _amount);
     }
 
     // Update dev address by the previous dev.
@@ -372,9 +374,31 @@ contract MasterMrm is Ownable {
             address referrer = referral.getReferrer(_user);		
             uint256 commissionAmount = _pending.mul(referralCommissionRate).div(10000);		
             if (referrer != address(0) && commissionAmount > 0) {		
-                mrm.mint(referrer, commissionAmount);		
+                marm.mint(referrer, commissionAmount);		
                 emit ReferralCommissionPaid(_user, referrer, commissionAmount);		
             }		
         }		
     }
+    
+    //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
+    function updateEmissionRate(uint256 _marmPerBlock) public onlyOwner {
+        massUpdatePools();
+        marmPerBlock = _marmPerBlock;
+        emit UpdateEmissionRate(msg.sender, _marmPerBlock);
+    }
+       
+        // Only update before start of farm
+
+    function setStartBlock(uint256 newStartBlock) external onlyOwner() {
+        massUpdatePools();
+        startBlock = newStartBlock;
+        emit SetStartBlock(msg.sender, newStartBlock);
+    }
+    
+//    function setMarmPerBlock(uint256 newMarmPerBlock) external onlyOwner() {
+//        massUpdatePools();
+//        marmPerBlock = newMarmPerBlock;
+//        emit SetMarmPerBlock(msg.sender, newMarmPerBlock);
+//    }
+        
 }
